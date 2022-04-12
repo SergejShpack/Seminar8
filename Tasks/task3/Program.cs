4bfc5c7972bdc0a887fec8507046996eeb90151e
Serj <nikolaev.sergej2018@yandex.ru>
2022-04-12 20:12:17 +0300

    t3

---
+++ Tasks/task3/Program.cs
@@ -0,0 +1,46 @@
+﻿// 3. Максимальный элемент матрицы
+
+void FillArray(int[,] arr, int startEl, int endEl)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(startEl, endEl + 1);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+
+            Console.Write(String.Format("{0,4}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MaxArrayElements(int[,] arr)
+{
+    int maxEl = arr[0, 0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] > maxEl) maxEl = arr[i, j];
+        }
+    }
+    return maxEl;
+}
+
+int n = 4, m = 5;   //Размер массива
+int minArrayElement = -100, maxArrayElement = 250;
+int[,] array = new int[n, m];
+
+FillArray(array, minArrayElement, maxArrayElement);
+Console.WriteLine("Данная матрица: ");
+PrintArray(array);
+
+Console.WriteLine($"Максимальный элемент: {MaxArrayElements(array)}");
