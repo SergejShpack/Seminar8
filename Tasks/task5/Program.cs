fd4bc4ac8a92c3628b50b738bf7d8b806cb228f0
Serj <nikolaev.sergej2018@yandex.ru>
2022-04-12 20:23:55 +0300

    t5

---
+++ Tasks/task5/Program.cs
@@ -0,0 +1,46 @@
+﻿// 5. Сумма элементов главной диагонали матрицы
+
+void FillArray(int[,] arr, int startEl, int endEl)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(startEl, endEl + 1);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            //Console.Write(arr[i, j] + " ");
+            Console.Write(String.Format("{0,4}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int SumArrayMainDiagonal(int[,] arr)
+{
+    int sumDiagonal = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sumDiagonal += arr[i, i];
+    }
+    return sumDiagonal;
+}
+
+int n = 4, m = 4;   //Размер массива
+int minArrayElement = -10, maxArrayElement = 10;
+int[,] array = new int[n, m];
+
+FillArray(array, minArrayElement, maxArrayElement);
+Console.WriteLine("Данная матрица: ");
+PrintArray(array);
+
+if (array.GetLength(0) == array.GetLength(1))
+    Console.WriteLine($"Сумма элементов главной диагонали: {SumArrayMainDiagonal(array)}");
+else
+    Console.WriteLine($"Матрица [{array.GetLength(0)}, {array.GetLength(1)}] не квадратная.");
