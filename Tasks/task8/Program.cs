6c74ebc80375aa8cf6f57c095e5a6e15a2cf936c
Serj <nikolaev.sergej2018@yandex.ru>
2022-04-12 20:38:12 +0300

    t8

---
+++ Tasks/task8/Program.cs
@@ -0,0 +1,46 @@
+﻿// 8. Минимальный элемент, расположенный на главной диагонали матрицы
+
+void FillArray(int[,] arr, int startEl, int endEl)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(startEl, endEl + 1);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            
+            Console.Write(String.Format("{0,4}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MinElArrayMainDiagonal(int[,] arr)
+{
+    int minElDiagonal = arr[0, 0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        if (arr[i, i] < minElDiagonal) minElDiagonal = arr[i, i];
+    }
+    return minElDiagonal;
+}
+
+int n = 5, m = 5;   //Размер массива
+int minArrayElement = -10, maxArrayElement = 10;
+int[,] array = new int[n, m];
+
+FillArray(array, minArrayElement, maxArrayElement);
+Console.WriteLine("Данная матрица: ");
+PrintArray(array);
+
+if (array.GetLength(0) == array.GetLength(1))
+    Console.WriteLine($"Минимальный элемент матрицы {MinElArrayMainDiagonal(array)}.");
+else
+    Console.WriteLine($"Матрица [{array.GetLength(0)}, {array.GetLength(1)}] не квадратная.");
