2d2fb05ba6e5f353a1a7098bb708167619573f51
Serj <nikolaev.sergej2018@yandex.ru>
2022-04-12 20:16:36 +0300

    t4

---
+++ Tasks/task4/Program.cs
@@ -0,0 +1,46 @@
+﻿// 4. Минимальный элемент матрицы
+
+void FillArray(int[,] arr, int startEl, int endEl)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(startEl, endEl + 1);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            
+            Console.Write(String.Format("{0,4}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MinArrayElements(int[,] arr)
+{
+    int minEl = arr[0, 0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] < minEl) minEl = arr[i, j];
+        }
+    }
+    return minEl;
+}
+
+int n = 4, m = 5;    //Размер массива
+int minArrayElement = -100, maxArrayElement = 250;
+int[,] array = new int[n, m];
+
+FillArray(array, minArrayElement, maxArrayElement);
+Console.WriteLine("Данная матрица: ");
+PrintArray(array);
+
+Console.WriteLine($"Минимальный элимент матрицы: {MinArrayElements(array)}");
