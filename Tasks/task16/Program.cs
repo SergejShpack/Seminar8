4b318ddc972ed55699947f69a5d2daa965798f4c
Serj <nikolaev.sergej2018@yandex.ru>
2022-04-12 21:18:25 +0300

    t16

---
+++ Tasks/task16/Program.cs
@@ -0,0 +1,32 @@
+﻿// 16. Печать матрицы на экран
+
+void FillArray(int[,] arr, int startEl, int endEl)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(startEl, endEl + 1);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            //Console.Write(arr[i, j] + " ");
+            Console.Write(String.Format("{0,4}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int n = 5, m = 5; //Размер матрицы
+int minArrayElement = -10, maxArrayElement = 10;
+int[,] array = new int[n, m];
+
+FillArray(array, minArrayElement, maxArrayElement);
+Console.WriteLine("Данная матрица: ");
+PrintArray(array);
+
