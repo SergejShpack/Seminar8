ce2c725f3e6987793e5ce4a6141ca3639d1de131
Serj <nikolaev.sergej2018@yandex.ru>
2022-04-12 20:29:09 +0300

    t6

---
+++ Tasks/task6/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// 6. Произведение элементов главной диагонали матрицы
+
+void FillArray(int[,] arr, int startEl, int endEl)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(startEl, endEl + 1);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            
+            Console.Write(String.Format("{0,4}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MultArrayMainDiagonal(int[,] arr)
+{
+    int multDiagonal = 1;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        multDiagonal *= arr[i, i];
+    }
+    return multDiagonal;
+}
+
+int n = 3, m = 3; //Размер массива
+int minArrayElement = -10, maxArrayElement = 10;
+int[,] array = new int[n, m];
+
+FillArray(array, minArrayElement, maxArrayElement);
+Console.WriteLine("Данная матрица: ");
+PrintArray(array);
+
+if (array.GetLength(0) == array.GetLength(1))
+    Console.WriteLine($"Произведение элементов диагонали матрицы: {MultArrayMainDiagonal(array)}.");
+else
+    Console.WriteLine($"Матрица [{array.GetLength(0)}, {array.GetLength(1)}] не квадратная.");
